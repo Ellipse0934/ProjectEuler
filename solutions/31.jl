283a4d66810224f247a522811018843f978ae1b5
Ellipse0934 <7466364+Ellipse0934@users.noreply.github.com>
2023-10-25 00:37:22 +0530

    Solution for :31

---
+++ solutions/31.jl
@@ -0,0 +1,27 @@
+function solve()
+    result = 0
+    for a in 0:1
+    for b in 0:2
+    for c in 0:4
+    for d in 0:10
+    for e in 0:20
+    for f in 0:40
+    for g in 0:100
+        if a * 200 +
+            b * 100 +
+            c * 50 +
+            d * 20 +
+            e * 10 +
+            f * 5 +
+            g * 2 <= 200
+                result += 1
+        end
+    end
+    end
+    end
+    end
+    end
+    end
+    end
+    result
+end
