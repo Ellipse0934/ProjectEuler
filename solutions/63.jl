7c6288812476c4523b788284d8b7d98ee8ec29f5
Ellipse0934 <7466364+Ellipse0934@users.noreply.github.com>
2024-07-05 11:29:29 +0530

    feat(63): remove bigint

--- solutions/63.jl
+++ solutions/63.jl
@@ -1,26 +1,15 @@
-#=
-# TODO: Don't use bigint for this problem
-=#
-
-function digits(n)
-    count = 1
-    while n >= 10
-        n ÷= 10
-        count += 1
-    end
-    count
-end
-
-function f()
-    count = 0
-    for i in 1:10
+function solve()
+    count = 1 
+    for i in 1:9
         j = 1
         for j in 1:100
-            n = big(i) ^ j
-            if j == digits(n) 
+            digits = ceil(Int, j *log10(i))
+            if j == digits
                 count += 1
             end
         end
     end
     count 
 end
+
+solve()
